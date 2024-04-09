d86f00758117a7064ab904e53797a7f3f17e453e
surya <surya@gmail.com>
2024-04-09 09:52:09 +0530

    9-4-2024

--- FundTransfer/src/com/pay/surya/service/UserService.java
+++ FundTransfer/src/com/pay/surya/service/UserService.java
@@ -8,15 +8,15 @@ public interface UserService {
 
 	public void createAccount();
 
-	public void Logout();
+	public void Logout(User user);
 
 	public void MyAccountDetail();
 
 	public void AccountActivity();
 
-	public void FundTransfer();
+	public void FundTransfer(int amount,int pin , User fromUser, User toUser);
 
-	public void withdraw();
+	public void withdraw(User user);
 
 	public void ChangePin();
 	
--- FundTransfer/src/com/pay/surya/transfer/AccountFundTransfer.java
+++ FundTransfer/src/com/pay/surya/transfer/AccountFundTransfer.java
@@ -47,74 +47,95 @@ public class AccountFundTransfer implements UserService {
 
 		int menuChoice = scanner.nextInt();
 		if (menuChoice == 1) {
-			this.login();
+			if (activeAcc == 1) {
+				Logout(user1);
+			} else if (activeAcc == 2) {
+				Logout(user2);
+			} else {
+				this.login();
+			}
+		}
 
-		} else {
-			this.createAccount();
+		else if (menuChoice == 2) {
+			if (activeAcc != 0) {
+				if (activeAcc == 1) {
+					accountInfo(user1);
+
+				} else if (activeAcc == 2) {
+
+					accountInfo(user2);
+				}
+
+			} else {
+				createAccount();
+			}
+
+		}
+
+		else if (menuChoice == 3) {
+			if (activeAcc == 1) {
+				print(user1.getHistory());
+			} else {
+				print(user2.getHistory());
+			}
+			mainMenu();
+		}
+
+		else if (menuChoice == 4) {
+			if (activeAcc == 1) {
+				FundTransfer(100, 123456, user1, user2);
+
+			}
 		}
 
 	}
 
 	@Override
-	public void login() { 
+	public void login() {
 		System.out.println("Welcome to bank");
 		System.out.println("Eneter Your bank Account Number");
-		String accNumber=scanner.next();
-		
-		if(accNumber.equalsIgnoreCase(user1.getAcccountNumber()))
-		{
+		String accNumber = scanner.next();
+
+		if (accNumber.equalsIgnoreCase(user1.getAcccountNumber())) {
 			System.out.println("Eneter 6 digit Pin !!!");
 			int pin = scanner.nextInt();
-			if(ValidateUser.verifyPin(pin, user1))
-			{
-			activeAcc=1;	
-			System.out.println("!! Login Successfull   !!");
-			createLog(user1, "Account Login");
-			mainMenu();
-			}
-			else
-			{
-				System.out.println("!! Wrong Pin Try Again !!");
-				login();
-			}
-		}
-		
-		else if(accNumber.equalsIgnoreCase(user2.getAcccountNumber()))
-		{
-			
-			System.out.println("Eneter 6 digit Pin !!!");
-			int pin = scanner.nextInt();
-			if(ValidateUser.verifyPin(pin, user2))
-			{
-			activeAcc=2;	
-			System.out.println("!! Login Successfull   !!");
-			createLog(user2, "Account Login");
-			mainMenu();
-			}
-			else
-			{
+			if (ValidateUser.verifyPin(pin, user1)) {
+				activeAcc = 1;
+				System.out.println("!! Login Successfull   !!");
+				createLog(user1, "Account Login");
+				mainMenu();
+			} else {
 				System.out.println("!! Wrong Pin Try Again !!");
 				login();
 			}
 		}
 
-		
+		else if (accNumber.equalsIgnoreCase(user2.getAcccountNumber())) {
+
+			System.out.println("Eneter 6 digit Pin !!!");
+			int pin = scanner.nextInt();
+			if (ValidateUser.verifyPin(pin, user2)) {
+				activeAcc = 2;
+				System.out.println("!! Login Successfull   !!");
+				createLog(user2, "Account Login");
+				mainMenu();
+			} else {
+				System.out.println("!! Wrong Pin Try Again !!");
+				login();
+			}
+		}
+
 	}
 
 	@Override
 	public void createAccount() {
-		
-		if(user1.getUserName()==null)
-		{
-			user=1;
-		}
-		else if(user2.getUserName()==null)
-		{
-			user=2;
-			
-		}
-		else
-		{
+
+		if (user1.getUserName() == null) {
+			user = 1;
+		} else if (user2.getUserName() == null) {
+			user = 2;
+
+		} else {
 			System.out.println("!! OOps Only 2 user Can be created...");
 		}
 
@@ -163,52 +184,45 @@ public class AccountFundTransfer implements UserService {
 		System.out.println("1.Saving ");
 		System.out.println("2.Current");
 		int accountType = scanner.nextInt();
-		if(accountType!= 0 && accountType<=2)
-		{
-		  if(accountType==1)
-			  accType="Saving";
-		  else
-			  accType="Current";
-		}
-		else
-		{
+		if (accountType != 0 && accountType <= 2) {
+			if (accountType == 1)
+				accType = "Saving";
+			else
+				accType = "Current";
+		} else {
 			print("[!! Account Type is Not Valid !! ]");
 		}
-		
+
 		System.out.println("====|    Enter Amount You want to Save   |====");
 		int amount = scanner.nextInt();
-		if(amount<0)
-		{
-		print("[!! Sorry You Can Not Open an Account With 0(Zero) !! ]");	
+		if (amount < 0) {
+			print("[!! Sorry You Can Not Open an Account With 0(Zero) !! ]");
 		}
-		
+
 		System.out.println("====|  Create 6 Digit Pin    |====");
 		int pin = scanner.nextInt();
-		if (!ValidateUser.checkLength(6, String.valueOf(pin),true)) {
+		if (!ValidateUser.checkLength(6, String.valueOf(pin), true)) {
 			print("[!! Pin Must Be 6 Digit !! ]");
 			createAccount();
 		}
-		
+
 		System.out.println("====|   Generating 11 Digit Account Number    |====");
 		String acNum = Utils.generateAcNum();
-		if(user==1)
-		{
-		user1.setAcccountNumber(acNum);
-		user1.setUserName(name);
-		user1.setAccountBalance(amount);
-		user1.setBankName(bankName);
-		user1.setAccountPin(pin);
-		user1.setEmail(email);
-		user1.setType(accType);
-		user1.setAcccountNumber(acNum);
-		user1.setHistory(Utils.getTimestamp());
-		user1.setIfscCode(ifsc);
-		user1.setMobile(mobile);
-		this.createLog(user1, " Account Created "); 
-	    this.accountInfo(user1);
-		}
-		else
-		{
+		if (user == 1) {
+			user1.setAcccountNumber(acNum);
+			user1.setUserName(name);
+			user1.setAccountBalance(amount);
+			user1.setBankName(bankName);
+			user1.setAccountPin(pin);
+			user1.setEmail(email);
+			user1.setType(accType);
+			user1.setAcccountNumber(acNum);
+			user1.setHistory(Utils.getTimestamp());
+			user1.setIfscCode(ifsc);
+			user1.setMobile(mobile);
+			this.createLog(user1, " Account Created ");
+			this.accountInfo(user1);
+		} else {
 			user2.setAcccountNumber(acNum);
 			user2.setUserName(name);
 			user2.setAccountBalance(amount);
@@ -220,15 +234,12 @@ public class AccountFundTransfer implements UserService {
 			user2.setHistory(Utils.getTimestamp());
 			user2.setIfscCode(ifsc);
 			user2.setMobile(mobile);
-			this.createLog(user2, " Account Created "); 
-		    this.accountInfo(user2);
+			this.createLog(user2, " Account Created ");
+			this.accountInfo(user2);
 
-			
 		}
 	}
 
-	
-	
 	private void accountInfo(User user) {
 		System.out.println("-----------*******-------------");
 		System.out.println("-----------***[ Account Created Successfully ]***-------------");
@@ -244,7 +255,7 @@ public class AccountFundTransfer implements UserService {
 		System.out.println("!!~ Account Pin => " + user.getAccountPin());
 		print("-----------*******-------------");
 		this.mainMenu();
- 	 	  	
+
 	}
 
 	private void print(String string) {
@@ -253,7 +264,7 @@ public class AccountFundTransfer implements UserService {
 	}
 
 	@Override
-	public void Logout() {
+	public void Logout(User user) {
 		// TODO Auto-generated method stub
 
 	}
@@ -271,14 +282,56 @@ public class AccountFundTransfer implements UserService {
 	}
 
 	@Override
-	public void FundTransfer() {
-		// TODO Auto-generated method stub
+	public void FundTransfer(int amount, int pin, User fromUser, User toUser) {
+		if (ValidateUser.verifyPin(pin, fromUser)) {
+			if (amount <= fromUser.getAccountBalance()) {
+				toUser.setAccountBalance(toUser.getAccountBalance() + amount);
+				fromUser.setAccountBalance(fromUser.getAccountBalance() - amount);
+				print("---------***[Fund Transfer Succesfully]*****----------");
+				System.out.println("!! ~ Available Balance =: " + fromUser.getAccountBalance());
+				createLog(fromUser, amount + "Transfer to" + toUser.getUserName());
+				createLog(toUser, amount + "Recevied from " + fromUser.getUserName());
+				mainMenu();
+
+			} else {
+				System.out.println("!!~Not Enough Balalnce in Account ");
+				System.out.println("----------------------------------");
+				mainMenu();
+			}
+		} else {
+			System.out.println("!!! Your Pin In Incorrect..  !!!");
+		}
 
 	}
 
 	@Override
-	public void withdraw() {
-		// TODO Auto-generated method stub
+	public void withdraw(User user) {
+		System.out.println("!!~ Enter Amount");
+		int amount = scanner.nextInt();
+
+		System.out.println("!!~ Enter 6 Digit Pin");
+		int pin = scanner.nextInt();
+		if (ValidateUser.verifyPin(pin, user)) {
+			if (amount <= user.getAccountBalance()) {
+
+				user.setAccountBalance(user.getAccountBalance() - amount);
+				print("---------***[withdraw Succesfully]*****----------");
+				System.out.println("!! ~ Available Balance =: " + user.getAccountBalance());
+				createLog(user, amount + "withdraw");
+				mainMenu();
+			} else {
+				System.out.println("!!~Not Enough Balalnce in Account ");
+				System.out.println("----------------------------------");
+				mainMenu();
+
+			}
+
+		}
+
+		else {
+			System.out.println("[!!Pin is Not Valid !!]");
+			mainMenu();
+		}
 
 	}
 
@@ -290,18 +343,15 @@ public class AccountFundTransfer implements UserService {
 
 	@Override
 	public void createLog(User user, String msg) {
-		 
-		String history=null;
-		if(user.getHistory()==null)
-		{
-			history="";
+
+		String history = null;
+		if (user.getHistory() == null) {
+			history = "";
+		} else {
+			history = user.getHistory();
+
 		}
-		else
-		{
-			history=user.getHistory();
-			
-		}
-		user.setHistory(msg+" on "+Utils.getTimestamp()+"\n"+history);
+		user.setHistory(msg + " on " + Utils.getTimestamp() + "\n" + history);
 	}
 
 }
